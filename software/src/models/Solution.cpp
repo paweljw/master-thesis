d3bb4efd9539689f33b88332a3297e40bcedba9b
Paweł J. Wal <pjw@paweljw.eu>
2014-08-31 13:35:20 +0200

    Updated Solution model
    Added proper accessors, created some constants

--- software/src/models/Solution.cpp
+++ software/src/models/Solution.cpp
@@ -1,24 +1,64 @@
-#include "solution.hpp"
+#include "Solution.hpp"
 
-namespace pjw
+namespace horizon
 {
 	namespace models
 	{
 
 		Solution::Solution()
 		{
-			this->ID = 0;
-			this->State = 0;
-			this->CurrentWave = 0;
-			this->Solution = "";
+			setID(0);
+			setState(SOLUTION_NOT_STARTED);
+			setWave(0);
+			setSolution(0);
 		}
 
 		Solution::Solution(int _ID)
 		{
+			setID(_ID);
+			setState(SOLUTION_NOT_STARTED);
+			setWave(0);
+			setSolution(0);
+		}
+
+		int Solution::getID()
+		{
+			return this->ID;
+		}
+
+		void Solution::setID(int _ID)
+		{
 			this->ID = _ID;
-			this->State = 0;
-			this->CurrentWave = 0;
-			this->Solution = "";
+		}
+
+		int Solution::getState()
+		{
+			return this->State;
+		}
+
+		void Solution::setState(int _State)
+		{
+			this->State = _State;
+		}
+
+		int Solution::getWave()
+		{
+			return this->CurrentWave;
+		}
+
+		void Solution::setWave(int _Wave)
+		{
+			this->CurrentWave = _Wave;
+		}
+
+		std::string Solution::getSolution()
+		{
+			return this->_Solution;
+		}
+
+		void Solution::setSolution(std::string __Solution)
+		{
+			this->_Solution = __Solution;
 		}
 	}
 }
--- software/src/models/Solution.hpp
+++ software/src/models/Solution.hpp
@@ -1,23 +1,39 @@
+#include <string>
+
 #pragma once
 
 #ifndef __solution_hpp
 #define __solution_hpp
 
-#include <string.h>
-
 namespace horizon
 {
 	namespace models
 	{
+		const int SOLUTION_NOT_STARTED	= 0;
+		const int SOLUTION_STARTED		= 1;
+		const int SOLUTION_COMPLETE		= 2;
+		const int SOLUTION_BROKEN		= 9;
+
 		class Solution
 		{
-			public:
-				Solution();
-				Solution(int);
-				int ID;
-				int State;
-				int CurrentWave;
-				std::string _Solution;
+		private:
+			int ID;
+			int State;
+			int CurrentWave;
+			std::string _Solution;
+
+		public:
+			Solution();
+			Solution(int);
+			int getID();
+			void setID(int);
+			int getState();
+			void setState(int);
+			int getWave();
+			void setWave(int);
+			std::string getSolution();
+			void setSolution(std::string);
+				
 		};
 	}
 }
