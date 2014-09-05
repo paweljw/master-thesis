0c600d35b51655ef18d9700be0565b725a50e4cb
Paweł J. Wal <pjw@paweljw.eu>
2014-09-05 23:02:37 +0200

    Completed splitter

--- software/src/splitter.cpp
+++ software/src/splitter.cpp
@@ -13,33 +13,66 @@
 #include <boost/filesystem.hpp>
 
 // SQLITE3 BINDINGS
-#include "lib/sqlite/sqlite3.h"
+#include "lib/inih/INIReader.h"
 
 // UTILITIES
 #include "utilhead.hpp"
+
+#include "models\Solution.hpp"
+#include "models\Wave.hpp"
+#include "models\Task.hpp"
+
+#include "ServerDatabaseAccessor.hpp"
+#include "ServerSQLiteDatabaseAccessor.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv) 
 {
+	// Check if we have arguments at all
 	if(argc == 1)
 	{
 		cout << "Usage: splitter matrix.mtx rhs.mtx [part size]" << endl;
 		return 0;
 	}
 
+	// Set up logging facilities
 	loggerhead("splitter");
+	using namespace logging::trivial;
+	src::severity_logger< severity_level > lg;
 
-    using namespace logging::trivial;
-    src::severity_logger< severity_level > lg;
+	// Set up configuration facilities
+	INIReader reader("server_config.ini");
+	
+	if (reader.ParseError() < 0) {
+		BOOST_LOG_SEV(lg, fatal) << "Can't load 'server_config.ini'\n";
+		return 1;
+	}
 
-	// 1.1 MTX file handle is opened
+	horizon::db::ServerDatabaseAccessor* DAO = new horizon::db::ServerSQLiteDatabaseAccessor(reader.Get("paths", "db_file", "storage/server.s3db"));
+
+	// Read configuration defaults
+
+	int PartSize = reader.GetInteger("matrix", "part_size", 1024);
+
+	if (argc > 3)
+	{
+		string sPartSize = argv[3];
+		istringstream isPartSize(sPartSize);
+		isPartSize >> PartSize;
+	}
+
+	std::string STORAGE_DIR = reader.Get("paths", "storage", "storage/");
+	std::string TMP_DIR = reader.Get("paths", "tmp", "tmp/");
+
+	// Check for MTX file name
 	if(argc < 2)
 	{
 		BOOST_LOG_SEV(lg, fatal) << "MTX file name is required";
 		return 1;
 	}
 
+	// Open MTX handle
 	BOOST_LOG_SEV(lg, info) << "Opening " << argv[1];
 
 	ifstream MTX(argv[1], ios_base::in);
@@ -50,6 +83,7 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
+	// Load matrix dimensions
 	int X, Y, entries;
 	string line;
 
@@ -63,47 +97,19 @@ int main(int argc, char** argv)
 		}
 	}
 	
+	// Check if universe is still sane
 	if(X != Y)
 	{
 		BOOST_LOG_SEV(lg, fatal) << "Matrix not square, " << X << "!=" << Y;
 		return 2;
 	}
 
+	#pragma region OpenRHS
 	BOOST_LOG_SEV(lg, info) << "Reading MTX at " << X << " by " << Y << ", " << entries << " entries";
 
 	int OnePercent = entries / 100;
-	int PartSize = 1024;
 
-	/// TODO: Read RHS filename from argv[2]
-
-	if(argc > 3)
-	{
-		string sPartSize = argv[3];
-		istringstream isPartSize(sPartSize);
-		isPartSize >> PartSize;
-	}
-
-	BOOST_LOG_SEV(lg, info) << "Part size at " << PartSize;
-
-	int PartsNum = X / PartSize + (X%PartSize ? 1 : 0);
-
-	BOOST_LOG_SEV(lg, info) << "Creating " << PartsNum << " parts";
-
-	vector<ofstream> OutStreams(PartsNum);
-
-	string base_filename = "package_" + formattedNow();
-
-	for(int i=0; i<PartsNum;i++)
-	{
-		stringstream FileName;
-		FileName << base_filename << "_" << i << ".mcx";
-
-		OutStreams[i] = ofstream(FileName.str());
-	}
-
-	// Now that we have parts and what not figured out, we need to get the RHS
-
-	if(argc < 3)
+	if (argc < 3)
 	{
 		BOOST_LOG_SEV(lg, fatal) << "RHS file name is required";
 		return 1;
@@ -111,7 +117,7 @@ int main(int argc, char** argv)
 
 	ifstream RHS(argv[2], ios_base::in);
 
-	if(!RHS.good() || RHS.bad() || !RHS.is_open())
+	if (!RHS.good() || RHS.bad() || !RHS.is_open())
 	{
 		BOOST_LOG_SEV(lg, fatal) << "Unable to open RHS file";
 		return 1;
@@ -119,9 +125,9 @@ int main(int argc, char** argv)
 
 	int _X, _Y;
 
-	while(getline(RHS, line))
+	while (getline(RHS, line))
 	{
-		if(line[0] != '%')
+		if (line[0] != '%')
 		{
 			istringstream iss(line);
 			iss >> _X >> _Y;
@@ -129,12 +135,47 @@ int main(int argc, char** argv)
 		}
 	}
 
-	if(X != _X)
+	if (X != _X)
 		BOOST_LOG_SEV(lg, warning) << "RHS size not equal to matrix dimensions";
 
 	int rhscounter = 0;
 	int OneXPercent = X / 100;
+	#pragma endregion
 
+	horizon::models::Solution solution_;
+	/// TODO: Need to sanitize this a bit
+	solution_.setName(argv[1]);
+
+	DAO->RegisterSolution(solution_);
+
+	horizon::models::Wave wave_;
+	wave_.setSeq(1);
+	wave_.setSolutionID(solution_.getID());
+
+	DAO->RegisterWave(wave_);
+
+	solution_.setWave(wave_.getID());
+	DAO->UpdateSolution(solution_);
+
+	BOOST_LOG_SEV(lg, info) << "Part size at " << PartSize;
+
+	// Create parts handles
+	int PartsNum = X / PartSize + (X%PartSize ? 1 : 0);
+
+	BOOST_LOG_SEV(lg, info) << "Creating " << PartsNum << " parts";
+
+	vector<ofstream> OutStreams(PartsNum);
+
+	string base_filename = boost::lexical_cast<std::string>(wave_.getID());
+
+	for(int partnum=0; partnum<PartsNum;partnum++)
+	{
+		std::string FileName = base_filename + "_" + boost::lexical_cast<std::string>(partnum) + ".mcx";
+
+		OutStreams[partnum] = ofstream(TMP_DIR + FileName);
+	}
+
+	// Write out RHS to package files
 	BOOST_LOG_SEV(lg, info) << "Writing out RHS to packages";
 
 	cout << "Focus on one hundred pretty dots:" << endl;
@@ -161,6 +202,7 @@ int main(int argc, char** argv)
 	double lvalue;
 	int count = 0;
 
+	// Write out MTX contents to packages
 	BOOST_LOG_SEV(lg, info) << "Writing out MTX to packages";
 
 	cout << "Focus on one hundred even prettier dots:" << endl;
@@ -200,15 +242,21 @@ int main(int argc, char** argv)
 
 	for(int i=0; i<PartsNum;i++)
 	{
-		stringstream FileName;
-		FileName << base_filename << "_" << i << ".mcx";
+		horizon::models::Task task_;
+		task_.setName(solution_.getName() + " wave " + boost::lexical_cast<std::string>(wave_.getSeq()) + " part " + boost::lexical_cast<std::string>(i));
+		task_.setNode("");
+		task_.setPartNum(i);
+		task_.setState(horizon::models::TASK_NOT_READY);
+		task_.setType(horizon::models::TASK_PART_SOLUTION);
+		task_.setWave(wave_.getID());
+		DAO->RegisterTask(task_);
 
-		string originalFilename = FileName.str();
+		std::string FileName = base_filename + "_" + boost::lexical_cast<std::string>(i);
 
-		ifstream file(originalFilename, ios_base::in);
-		BOOST_LOG_SEV(lg, info) << "Compressing " << originalFilename;
+		ifstream file(TMP_DIR + FileName + ".mcx", ios_base::in);
+		BOOST_LOG_SEV(lg, info) << "Compressing " << FileName;
 
-		ofstream outf(originalFilename + ".gz", ios_base::out|ios_base::binary);
+		ofstream outf(STORAGE_DIR + FileName + ".gz", ios_base::out|ios_base::binary);
 		boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
 		in.push(boost::iostreams::gzip_compressor());
 		in.push(file);
@@ -218,8 +266,19 @@ int main(int argc, char** argv)
 		outf.flush();
 		outf.close();
 
-		boost::filesystem::remove(originalFilename);
+		task_.markReady();
+		DAO->UpdateTask(task_);
 
-		/// TODO: Register packages here
+		wave_.incrementTasks();
+
+		boost::filesystem::remove(TMP_DIR + FileName + ".mcx");
 	}
+
+	wave_.markAvailable();
+	solution_.markAvailable();
+
+	DAO->UpdateSolution(solution_);
+	DAO->UpdateWave(wave_);
+
+	return 0;
 }
