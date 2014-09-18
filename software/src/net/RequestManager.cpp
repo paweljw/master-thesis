ab8d28959b00db83fdba7a7568ebe1e2637a2652
Paweł J. Wal <pjw@paweljw.eu>
2014-09-18 14:21:30 +0200

    Prepare server for request handling
    Added a router harness, basic logging and request fulfillment functions,
    created basic authentication

---
+++ software/src/net/RequestManager.cpp
@@ -0,0 +1,99 @@
+#include <string>
+#include <list>
+#include "lib/inih/INIReader.h"
+#include "net/RequestManager.hpp"
+
+using namespace logging::trivial;
+src::severity_logger< severity_level > lg;
+
+namespace horizon
+{
+	namespace net
+	{
+		INIReader reader("server_config.ini");
+
+		void request_hello(struct mg_connection *conn)
+		{
+			std::string uri = std::string(conn->uri);
+			boost::trim_left_if(uri, boost::is_any_of("/"));
+
+			std::string s = "Hello world!";
+			mg_printf_data(conn, "%s", s.c_str());
+
+			BOOST_LOG_SEV(lg, info) << "HLW " << uri << ": " << __FUNCTIONW__ << "+" << __LINE__;
+		}
+
+		void request_static(struct mg_connection *conn)
+		{
+			BOOST_LOG_SEV(lg, info) << "STC " << conn->uri << ": " << __FUNCTIONW__ << "+" << __LINE__;
+		}
+
+		int request_package(struct mg_connection *conn)
+		{
+			std::string code = get_param(parse_querystring(conn), "auth");
+
+			if(auth(code))
+			{
+				BOOST_LOG_SEV(lg, info) << "PKGA " << conn->uri << ": " << __FUNCTIONW__ << "+" << __LINE__;
+				mg_send_file(conn, ("./storage/" + std::string(conn->uri)).c_str());
+				return MG_MORE;
+			} else {
+				BOOST_LOG_SEV(lg, info) << "PKGU " << conn->uri << " (" << code << ") : " << __FUNCTIONW__ << "+" << __LINE__;
+				std::string s = "Unathorized " + code;
+				mg_printf_data(conn, "%s", s.c_str());
+				return MG_TRUE;
+			}
+		}
+
+		Parameters parse_querystring(struct mg_connection *conn)
+		{
+			Parameters ret = Parameters(0);
+
+			if(conn->query_string == NULL) return ret;
+
+			std::list<std::string> tokenList;
+
+			boost::split(tokenList, conn->query_string, boost::is_any_of("=&"), boost::token_compress_on);
+
+			for ( std::list<std::string>::iterator it = tokenList.begin();
+				it != tokenList.end();
+				++it)
+			{
+				std::string param, value;
+				param = *it;
+				++it;
+				value = *it;
+				ret.push_back( std::make_pair(param, value) );
+			}
+
+			return ret;
+		}
+
+		std::string get_param(Parameters p, std::string param)
+		{
+			for(Parameters::iterator it = p.begin();
+				it != p.end();
+				++it)
+			{
+				if((*it).first == param)
+					return (*it).second;
+			}
+
+			return "";
+		}
+
+		bool auth(std::string in)
+		{
+			int many = reader.GetInteger("auth", "codes", 0);
+
+			for(int i=0; i<many; i++)
+			{
+				if (in == reader.Get("auth", "code" + boost::lexical_cast<std::string>(i), ""))
+					return true;
+			}
+
+			return false;
+		}
+	}
+
+}
---
+++ software/src/net/RequestManager.hpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <utility>
+#include "lib/mongoose/mongoose.h"
+#include <boost/algorithm/string.hpp>
+#include "utilhead.hpp"
+
+#pragma once
+
+#ifndef __REQUEST_MANAGER_INCLUDED
+#define __REQUEST_MANAGER_INCLUDED
+
+namespace horizon
+{
+	namespace net
+	{
+		typedef std::vector< std::pair<std::string, std::string> > Parameters;
+
+		/// Fulfills /hello request
+		void request_hello(struct mg_connection *conn);
+
+		/// Fulfills /*.mcx.gz?auth=code requests
+		int request_package(struct mg_connection *conn);
+
+		/// Logs static requests
+		void request_static(struct mg_connection *conn);
+
+		Parameters parse_querystring(struct mg_connection *conn);
+		std::string get_param(Parameters, std::string);
+
+		bool auth(std::string in);
+
+		static int router(struct mg_connection *conn, enum mg_event ev) 
+		{
+			std::string uri;
+
+			switch (ev)
+			{
+			case MG_AUTH:
+				return MG_TRUE;   // Authorize all requests
+			case MG_REQUEST:
+				// First handle special cases
+				if(boost::find_first(conn->uri, "/hello")) {
+					request_hello(conn);
+					return MG_TRUE;
+				} else if(boost::find_first(conn->uri, ".mcx.gz")) {
+					return request_package(conn);
+				}
+
+				request_static(conn);
+				return MG_FALSE;
+				break;
+			default:
+				return MG_FALSE;  // Rest of the events are not processed
+			}
+		}
+	}
+}
+
+#endif
