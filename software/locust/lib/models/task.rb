df72c4d5ee0da967fe01465bf7aef5e34f08a370
Paweł J. Wal <p@paweljw.al>
2015-04-06 15:24:21 +0200

    updates to task

--- software/locust/lib/models/task.rb
+++ software/locust/lib/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ActiveRecord::Base
   enum kind: [ :undefined, :solution, :reduce ]
   enum state: [ :not_ready, :ready, :sent, :received, :provisioned, :started, :processed, :complete, :broken ]
+
+  scope :undone, -> { where("state <= 5") }
+  scope :for_provisioning, -> { where("state = 3") }
 end
